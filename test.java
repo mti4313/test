b05abef062a3fd6550fe27d02b8d4a4722f06ef8
mti <matsuikazuhiro4@gmail.com>
2014-08-18 17:55:50 +0900

    test.javaをコミットしてみる

---
+++ test.java
@@ -0,0 +1,10 @@
+public class shiorin {
+  public static void main (String[] args) {
+    for(int i=0;i<100;i++)
+    {
+    System.out.print("o<");
+    System.out.println();
+    }
+    System.out.println("Happy Birthday Shiorin*");
+  }
+}
